41a250b06fab6a569df7976aec48ae62cfc30dcd
preetnair985 <preetinair985@gmail.com>
2019-10-23 12:30:46 +0530

    Removed Previous Files and added new files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
-import "./App.css";
-import PostList from "./component/PostList";
+import React, { Component } from "react";
+import LoginBox from "./component/LoginBox";
+import RegisterBox from "./component/RegisterBox";
+import '../src/css/Style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PostList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false
+    };
+  }
+
+  render() {
+    return (
+      <div className="box-container">
+        {this.state.isLoginOpen && <LoginBox />}
+        {this.state.isRegisterOpen && <RegisterBox />}
+      </div>
+    );
+  }
 }
 
 export default App;
